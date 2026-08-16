fd69b346b0926c75c035e8dbf3afaea84634cb86
together5566 <90299668+together5566@users.noreply.github.com>
2026-08-16 15:27:38 +0800

    chore(legend-sp): label custom build

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const APP_VER='v1.4.7';
+export const APP_VER='v1.4.7-LegendSP';
 export const OFFICIAL_URL='https://www.yakyolife.com/';
 export const OFFICIAL_HOST=OFFICIAL_URL.replace(/^https?:\/\//,'').replace(/\/$/,'');
